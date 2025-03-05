9348409ed8e14fafc3eca5fb3fb35239b9890190
pyshkovni <guess.the.tutor@gmail.com>
2025-03-05 11:37:05 +0300

    Добавили TOKEN в main

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
+from config import TOKEN
+
 
 # Экземпляр бота и диспетчера
-bot = Bot(token="Your Token")
+bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
 
 # Бот принимает команды, например /start.
 # Создадим хендлер - обработчик сообщений, и будем возвращать сообщение
@@ -16,6 +19,7 @@ async def process_start_command(message):
 @dp.message()
 async def echo_message(message):
     await message.answer(message.text)
+
 
 # функция запуска проекта
 async def main():
